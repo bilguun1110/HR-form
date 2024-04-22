8b3787a59723193b3ef6afaa383d5c53b0caca9d
binderiyabilguun@gmail.com <binderiyabilguun@gmail.com>
2024-04-22 11:40:28 +0800

    get

--- backend/quaries/form/getAllForm.ts
+++ backend/quaries/form/getAllForm.ts
@@ -2,8 +2,9 @@ import { FormModel } from "../../db";
 import { Request } from "express";
 
 export const getAllForm = async (req: Request) => {
+  const { filter = {} } = req.body;
   try {
-    const forms = await FormModel.find();
+    const forms = await FormModel.find(filter);
 
     if (forms === null) {
       throw new Error("not found form");
